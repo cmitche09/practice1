771c2b9b2baa3814b5c8b6efcea25d699bc29517
cmitche09 <cmitche09@gmail.com>
2020-09-01 19:12:37 -0400

    September 1st, 2020 Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        //Update: September 1st, 2020
+        String message = "This is Corey Mitchell. I just learned how to push my file to GitHub. Hopefully you like it!";
+        System.out.println(message);
     }
 }
